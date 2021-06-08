b0c052f6bc4cc964a87e0d0859e53e6f8935cf90
Lazard <trislaz@hotmail.fr>
2021-06-08 10:43:19 +0200

    uncommenting the main_tiling.py

--- scripts/main_tiling.py
+++ scripts/main_tiling.py
@@ -7,19 +7,19 @@ from glob import glob
 
 args = get_arguments()
 outpath = os.path.join(args.path_outputs, args.tiler, f'level_{args.level}')
-#if os.path.isfile(args.path_wsi):
-#    it = ImageTiler(args=args)
-#    it.tile_image()
-#else:
-#    dirs = []
-#    extensions = set(['.ndpi', '.svs', '.tif'])
-#    all_files = glob(os.path.join(args.path_wsi, '*.*'))
-#    files = [f for f in all_files if os.path.splitext(f)[1] in extensions]
-#    assert len(files) > 0, f"No files with extension {extensions} in {args.path_wsi}"
-#    for f in files:
-#        args.path_wsi = f
-#        it = ImageTiler(args=args)
-#        it.tile_image()
+if os.path.isfile(args.path_wsi):
+    it = ImageTiler(args=args)
+    it.tile_image()
+else:
+    dirs = []
+    extensions = set(['.ndpi', '.svs', '.tif'])
+    all_files = glob(os.path.join(args.path_wsi, '*.*'))
+    files = [f for f in all_files if os.path.splitext(f)[1] in extensions]
+    assert len(files) > 0, f"No files with extension {extensions} in {args.path_wsi}"
+    for f in files:
+        args.path_wsi = f
+        it = ImageTiler(args=args)
+        it.tile_image()
 
 #PCA
 if args.tiler != 'simple':
